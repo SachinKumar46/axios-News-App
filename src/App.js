d67998d75edf51a39834a961a378bd8e2025020a
Sachin Rajput <sachinrajput17798@gmail.com>
2024-06-26 15:17:03 +0530

    add some changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import FetchApi from "./components/FetchApi";
+import Footer from "./components/Footer";
+import Nabvar from "./components/Nabvar";
 
 function App() {
   return (
+    <>
+    <Nabvar/>
     <FetchApi/>
+    <Footer/>
+    </>
+    
   );
 }
 
--- src/components/FetchApi.js
+++ src/components/FetchApi.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import axios, { Axios } from 'axios'
+import axios from 'axios'
 
 export default function FetchApi() {
   const [news, setNews] = useState([])
@@ -16,28 +16,37 @@ export default function FetchApi() {
   return (
     <>
       <div className="container">
-        <button className="btn btn-primary mt-5" onClick={fetchApiData}>Fetch Api</button>
+        <div className="card mt-5">
+          <div class="card" >
+            <div class="card-body">
+              <h5 class="card-title">Fetch data with the help of Axios package</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem tempore, recusandae nam dolorum nostrum sed! Possimus ex, natus pariatur sunt ab itaque obcaecati voluptatem tempora, magni similique ad quae libero!</p>
+              <button className="btn btn-primary" onClick={fetchApiData}>Fetch News</button>
+
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="container">
         <div className="row">
-        {
-          news.map((value)=>{
-            return(
-              <div className="col-4 g-4">
-              <div className="card" >
-                <img src={value.urlToImage} className="card-img-top" alt="..." />
-                <div className="card-body">
-                  <h5 className="card-title">{value.title}</h5>
-                  <p className="card-text">{value.description}</p>
-                  <a href="/" className="btn btn-primary w-75">{value.url}</a>
+          {
+            news.map((value) => {
+              return (
+                <div className="col-4 g-4">
+                  <div className="card" >
+                    <img src={value.urlToImage} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                      <h5 className="card-title">{value.title}</h5>
+                      <p className="card-text">{value.description}</p>
+                      <a href="/" className="btn btn-primary">Resource: {value.source.name}</a>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            )
-          })
-        }
-          
+              )
+            })
+          }
+
         </div>
       </div>
     </>
